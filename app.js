f762d0e7e9d30d861a5338824fef385f01d70107
Lu <220716@astanait.edu.kz>
2024-02-14 06:00:01 +0600

    Your commit message

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const session = require('express-session');
 const User = require('./models/user');
+const fetch = require('node-fetch'); 
 
 const app = express();
 const port = 3000;
@@ -12,16 +13,29 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
-const apiKey = 'fc1e127af9212921e0257e83ec25f717';
 
+const apiKey = 'fc1e127af9212921e0257e83ec25f717';
+const newsApiKey = 'ac89d2647153421abc38717738f50dd6'
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Function to generate a random 6-digit user ID
-function generateUserID() {
-  return Math.floor(100000 + Math.random() * 900000).toString();
-}
+app.post('/top-news', async (req, res) => {
+  const city = req.body.city;
+  if (!city) {
+    return res.status(400).send('City parameter is missing');
+  }
 
+  try {
+    const newsUrl = `https://newsapi.org/v2/top-headlines?q=${city}&apiKey=${newsApiKey}`;
+    const response = await fetch(newsUrl);
+    const newsData = await response.json();
+    res.json(newsData);
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+let existingHtml = "";
 app.get('/', (req, res) => {
   res.redirect('/login');
 });
@@ -34,7 +48,28 @@ app.get('/register', (req, res) => {
   res.render('register');
 });
 
-// Weather route
+app.get('/top-news', (req, res) => {
+  res.render('top-news', { newsData: null });
+});
+
+
+app.post('/top-news', async (req, res) => {
+  const city = req.body.city;
+  if (!city) {
+    return res.status(400).send('City parameter is missing');
+  }
+
+  try {
+    const newsUrl = `https://newsapi.org/v2/everything?q=${city}&apiKey=${newsApiKey}`;
+    const response = await fetch(newsUrl);
+    const newsData = await response.json();
+    res.render('top-news', { newsData: newsData });
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.get('/weather', (req, res) => {
   res.render('weather', { weather: null, error: null });
 });
@@ -77,14 +112,12 @@ function fetchWeather(url, res) {
         weatherTextExpanded += `\nPressure: ${pressure} hPa`;
         weatherTextExpanded += `\nWind Speed: ${windSpeed} m/s`;
 
-        // Pass minTemp, maxTemp, pressure, and windSpeed to the weather template
-        res.render('weather', { weather: weatherTextExpanded, error: null, minTemp, maxTemp, pressure, windSpeed });
+        res.render('weather', { weather: weatherTextExpanded, error: null, minTemp, maxTemp, pressure, windSpeed});
+
       }
     }
   });
 }
-
-
 
 
 app.get('/admin', (req, res) => {
@@ -92,12 +125,10 @@ app.get('/admin', (req, res) => {
 });
 
 
-
-
-// Function to generate a random 6-digit user ID
 function generateUserID() {
   return Math.floor(100000 + Math.random() * 900000).toString();
 }
+
 function isAdmin(req, res, next) {
   const username = req.session.username;
   if (!username) {
@@ -117,12 +148,14 @@ app.post('/register', async (req, res) => {
   const { username, password } = req.body;
 
   try {
+    console.log('============ USER: ', User.toString(), User.create)
     const user = await User.create({
       username: username,
       password: password,
-      userID: generateUserID(), // Call the generateUserID function here
-      isAdmin: username === 'your_admin_username' // Set admin status based on username
+      userID: generateUserID(), 
+      isAdmin: username === 'admin' 
     });
+    
     res.render('success', { message: 'Registration successful', link: '/login' });
   } catch (error) {
     console.error('Registration error:', error);
@@ -131,17 +164,19 @@ app.post('/register', async (req, res) => {
 });
 
 
-// Login POST route
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
+    console.log(User.create)
     const user = await User.findOne({ username });
+    console.log(user, password)
     if (!user || user.password !== password) {
       return res.status(404).send('Invalid username or password');
     }
 
-    // Redirect to corresponding page based on user role
+    req.session.username = username;
+
     if (user.isAdmin) {
       res.redirect('/admin');
     } else {
@@ -154,7 +189,7 @@ app.post('/login', async (req, res) => {
 });
 
 
-// Create User Route
+
 app.post('/admin/create-user', isAdmin, async (req, res) => {
   const { username, password } = req.body;
 
@@ -162,8 +197,8 @@ app.post('/admin/create-user', isAdmin, async (req, res) => {
     const user = await User.create({
       username: username,
       password: password,
-      userID: generateUserID(), // Call the generateUserID function here
-      isAdmin: false // Set admin status to false as only admin can create admins
+      userID: generateUserID(), 
+      isAdmin: false 
     });
     res.render('admin', { message: 'User created successfully' });
   } catch (error) {
@@ -172,7 +207,7 @@ app.post('/admin/create-user', isAdmin, async (req, res) => {
   }
 });
 
-// Edit User Route
+
 app.post('/admin/edit-user', isAdmin, async (req, res) => {
   const { username, password } = req.body;
 
@@ -188,29 +223,29 @@ app.post('/admin/edit-user', isAdmin, async (req, res) => {
   }
 });
 
-// Delete User Route
+
 app.post('/admin/delete-user', isAdmin, async (req, res) => {
   const { username } = req.body;
+
+  console.log('delete', username)
 
   try {
     const user = await User.findOneAndDelete({ username });
     if (!user) {
       return res.status(404).send('User not found');
     }
-    res.render('admin', { message: 'User deleted successfully' });
+    return res.status(404).send('User deleted successfully');
+    return res.render('admin', { message: 'User deleted successfully' });
   } catch (error) {
     console.error('User deletion error:', error);
-    res.status(500).send('Internal Server Error');
+    return res.status(500).send('Internal Server Error');
   }
 });
 
-
-// Main page route (only accessible after login)
 app.get('/main', (req, res) => {
   res.render('main');
 });
 
-// Admin panel routes
 app.get('/admin', isAdmin, (req, res) => {
   res.render('admin');
 });
@@ -219,19 +254,17 @@ app.get('/admin', isAdmin, (req, res) => {
 function isAdmin(req, res, next) {
   const username = req.session.username;
   if (!username) {
-    return res.status(403).send('Access denied');
+    return res.status(403).send('Access denied. not logged');
   }
 
-  User.findOne({ username, isAdmin: true }, (err, user) => {
-    if (err || !user) {
-      return res.status(403).send('Access denied');
-    }
+  if(username === 'admin') {
     next();
-  });
+  } else {
+    return res.status(403).send('Access denied. not admin');
+  }
 }
 
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- models/user.js
+++ models/user.js
@@ -1,20 +1,31 @@
 //user.js
 const uri = "mongodb+srv://kamila:Sxx1LoKxE5baQ@cluster0.qje0zfw.mongodb.net/lab2?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const clientOptions = {serverApi: {version: '1', strict: true, deprecationErrors: true}};
 
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+mongoose.connect(uri, clientOptions);
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected successfully');
+
+    mongoose.connection.db.admin().command({ping: 1}, (err, result) => {
+        if (err) {
+            console.error('Error pinging MongoDB:', err);
+        } else {
+            console.log('MongoDB ping successful:', result);
+        }
+    });
 });
 
-client.connect();
+const User = mongoose.model('users', {
+    username: String,
+    password: String,
+    isAdmin: Boolean,
+    createdAt: Date,
+    updatedAt: Date,
+    userID: String,
+    deletedAt: Date
+});
 
-
-const db =client.db("lab2")
-const User= db.collection("users")
-
-module.exports = User;
+module.exports = User
